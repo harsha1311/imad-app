8eb674843cdf115ffabf86676d6611b874af594f
harsha1311 <harsha.mogali7@gmail.com>
2017-08-09 01:15:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/article-one',function(req,res)
 {
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/Article-two',function(req,res)
+app.get('/article-two',function(req,res)
 {
     res.send("Article two is requested");
 });
