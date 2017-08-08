8b3475100464a6b0dc0e00b9a671d44af9ea7a0d
harsha1311 <harsha.mogali7@gmail.com>
2017-08-09 02:02:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get('/', function (req, res) {
 });
 app.get('/:articleName',function(req,res)
 {
-    var articleName=req.params.articleName;
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 app.get('/ui/style.css', function (req, res) {
